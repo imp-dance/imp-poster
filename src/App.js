d33d1c5a04cdf2f55f132b88881f061d77bee959
IDAbyLIGL <haakonu97@gmail.com>
2019-02-28 21:52:04 +0100

    Setup boilerplate

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
